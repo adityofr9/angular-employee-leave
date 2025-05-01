cde21a36e2edb8f71710a8d71c7b8a45ce3159b3
adityofr9 <adityofr9@gmail.com>
2025-05-01 17:06:53 +0700

    update: adding cond

--- src/app/pages/admins/list/list.component.ts
+++ src/app/pages/admins/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/api/auth/auth.service';
 import { UserService } from 'src/app/api/user/user.service';
 import { environment } from 'src/environments/environment';
 
@@ -10,14 +11,17 @@ import { environment } from 'src/environments/environment';
 })
 export class ListComponent implements OnInit {
   apiEnv: any = environment.apiUrl;
+  currUser: any;
 
   list: any[] = [];
 
   constructor(
+    private auth: AuthService,
     private api: UserService
   ) { }
 
   ngOnInit() {
+    this.currUser = this.auth.user.value;
     this.getData();
   }
 

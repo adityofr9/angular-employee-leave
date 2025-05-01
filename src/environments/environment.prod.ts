6edc002ff1f510b56c579cd454177244ac1fa0af
adityofr9 <adityofr9@gmail.com>
2025-05-01 15:21:12 +0700

    feat: add mockapi url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://localhost:4200/api',
+    apiUrl: 'https://68132a37129f6313e2107072.mockapi.io/api',
     featureFlag: false
   };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:4200/api',
+    apiUrl: 'https://68132a37129f6313e2107072.mockapi.io/api',
     apiDummy: 'https://dummyjson.com',
     featureFlag: true
   };
